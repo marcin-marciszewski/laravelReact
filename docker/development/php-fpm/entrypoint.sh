8763c500739cf87b5cd32373dd856e0b66dae6cb
Marcin Marciszewski <marciszewski.m@gmail.com>
2025-04-06 09:53:24 +0200

    Add initial project files and update Docker configurations

--- docker/development/php-fpm/entrypoint.sh
+++ docker/development/php-fpm/entrypoint.sh
@@ -9,6 +9,8 @@ GROUP_ID=${GID:-1000}
 echo "Fixing file permissions with UID=${USER_ID} and GID=${GROUP_ID}..."
 chown -R ${USER_ID}:${GROUP_ID} /var/www || echo "Some files could not be changed"
 
+php artisan migrate --force
+
 # Clear configurations to avoid caching issues in development
 echo "Clearing configurations..."
 php artisan config:clear
